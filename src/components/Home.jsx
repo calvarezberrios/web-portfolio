294c1f281214d748f818f20ab99a151e32901796
Carlos Alvarez-Berrios <carlos.alvarezberrio@gmail.com>
2025-07-03 16:57:44 -0400

    Fix: media adjustments for proper alignment on mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -48,21 +48,20 @@ function Home() {
 export default Home;
 
 const Container = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    height: 83vh;
-    margin-bottom: 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-height: calc(100vh - 100px);
+  padding-top: 150px;
+  margin-bottom: 5rem;
 
-    @media(max-width: 1050px) {
-        margin-top: 10rem;
-        flex-direction: column;
-        justify-content: center;
-    }
-    @media(max-width: 480px) {
-        
-    }
-    
+  @media (max-width: 1050px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center; /* add this to center horizontally */
+    padding: 2rem;
+    gap: 3rem;
+  }
 `;
 
 const Intro = styled.div`
@@ -93,49 +92,61 @@ const Intro = styled.div`
 `;
 
 const ProfileImg = styled.div`
-    position: relative
-    width: 50%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  position: relative;
+  width: 420px;
+  height: 420px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 1050px) {
+    width: 320px;
+    height: 320px;
+    flex-shrink: 0;
+  }
+
+  @media (max-width: 480px) {
+    width: 280px;
+    height: 280px;
+  }
 `;
+
 
 const Circle = styled.div`
-    width: 500px;
-    height: 500px;
-    background: #1a1b1d;
-    border-radius: 50%;
-    z-index: 1;
-
-    @media(max-width: 1300px) {
-        width: 400px;
-        height: 400px;
-    }
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #1a1b1d;
+  border-radius: 50%;
+  z-index: 1;
 `;
+
+
+
 
 const Image = styled.img`
-    position: absolute;
-    height: 60%;
-    bottom: 15%;
-    border-radius: 38%;
-    z-index: 2;
-    filter: grayscale(100%) contrast(130%);
+  position: absolute;
+  height: 110%;
+  top: -12%;
+  z-index: 2;
+  filter: grayscale(100%) contrast(130%);
+  border-radius: 40%;
+  object-fit: contain;
 
-    @media(max-width: 1300px) {
-        height: 50%;
-        bottom: 21%;
-    }
+  @media (max-width: 1050px) {
+    height: 115%;
+    top: -14%;
+  }
 
-    @media(max-width: 1050px) {
-        bottom: -30%;
-    }
-
-    @media(max-width: 390px) {
-        bottom: -40%;
-    }
-
+  @media (max-width: 480px) {
+    height: 120%;
+    top: -18%;
+  }
 `;
+
+
 
 const Title = styled.h2`
     color: ${theme.colors.primary};
